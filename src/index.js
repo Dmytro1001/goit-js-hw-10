65ce55f23ff87603ed821348b2598a51255b2835
Dmytro1001 <31dmitri13@gmail.com>
2023-01-04 16:34:42 +0200

    trial version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import {fetchCountries, fetchSelectCountries} from "./js/fetchCountries";
+import Notiflix from 'notiflix';
+
+const searchBox = document.querySelector('input');
+const countryList = document.querySelector('.country-list');
+const countryInf = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+countryList.addEventListener('click', onClick);
+
+function onClick(e) {
+  const country = e.target.querySelector('span');
+  const nameCountry = country.textContent;
+
+  input.value = nameCountry;
+  
+  fetchSelectCountries(nameCountry)
+    .then(obj => {createMarkup(obj)})
+    .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
+}
+
+
+function onInput(e) {
+  const name = e.target.value;
+    if (!name) {
+      countryInfo.innerHTML = '';
+      countryList.innerHTML = '';
+    } else {
+        fetchCountries(name.trim())
+        .then(obj => {
+        if (obj.length >= 10) {
+            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            countryInfo.innerHTML = '';
+            countryList.innerHTML = '';
+        } else {createMarkup(obj)}
+        })
+        .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
+    }
+}
+
+function createMarkup(obj) {
+  if(obj.length >= 2){
+        const markup = obj.map(obj => 
+            `<li class="countries">
+              <p>
+              <img src="${obj.flags.svg}" alt="${obj.name.common}" width = "30"> &nbsp
+              <span>${obj.name.official}</span>
+              </p>
+            </li>`
+            );
+            countryList.innerHTML = markup.join('');
+            countryInf.innerHTML = '';
+        
+    } else {
+        const markup = obj.map(({ flags, name, capital, population, languages }) => 
+            `<div class="card">
+              <h2>
+                <img src="${flags.svg}" alt="${name.common}" width = "45"> &nbsp
+                ${name.official}
+              </h2>
+              <div>
+                <p><b>Capital:&nbsp</b><span>${capital}</span></p>
+                <p><b>Population:&nbsp</b><span>${population}</span></p>
+                <p><b>Languages:&nbsp</b><span>${Object.values(languages).join(", ")}</span></p>
+              </div>
+            </div>`
+            );
+            countryInf.innerHTML = markup.join('');
+            countryList.innerHTML = '';
+        }
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,27 @@
+
+function fetchCountries(country) {
+    const options = 'name, capital, population, flags, languages';
+    const BASE_URL = "https://restcountries.com/v3.1/name";
+
+    return fetch(`${BASE_URL}/${country}?fields=${options}`).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+
+        return resp.json();
+    });
+}
+
+function fetchSelectCountries(nameCountry) {
+    const options = 'name,capital,population,flags,languages';
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    return fetch(`${BASE_URL}/${nameCountry}?fields=${options}`)
+    .then(resp => {
+        if(!resp.ok){
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+    });
+}
+
+export  {fetchCountries, fetchSelectCountries};
